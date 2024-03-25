77658c52a374032a7afa98105cf8902dde1fbd91
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2024-03-25 16:13:20 +1000

    changed models and add them to admin panel

--- recipessite/recipes/admin.py
+++ recipessite/recipes/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Recipe, Category, RecipeCategory
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(RecipeCategory)
+class RecipeCategoryAdmin(admin.ModelAdmin):
+    pass
--- recipessite/recipes/models.py
+++ recipessite/recipes/models.py
@@ -18,14 +18,22 @@ class Recipe(models.Model):
     ○ Автор
     ○ *другие поля на ваш выбор, например ингредиенты и т.п.
     """
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    steps = models.TextField()
-    time = models.IntegerField()
-    image = models.ImageField(upload_to='recipes/')
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    changed_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=100, unique=True, verbose_name='Название')
+    description = models.TextField(max_length=1000, verbose_name='Описание')
+    steps = models.TextField(max_length=1000, verbose_name='Шаги приготовления')
+    time = models.TimeField(verbose_name='Время приготовления')
+    image = models.ImageField(upload_to='recipes/', verbose_name='Изображение', null=True, blank=True)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE, verbose_name='Автор')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    changed_at = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-changed_at']
+        verbose_name = 'рецепт'
+        verbose_name_plural = 'рецепты'
 
 
 class Category(models.Model):
@@ -34,8 +42,16 @@ class Category(models.Model):
     ○ Название
     ○ *другие поля на ваш выбор
     """
-    title = models.CharField(max_length=255)
-    description = models.TextField()
+    title = models.CharField(max_length=255, unique=True, verbose_name='Название')
+    description = models.TextField(max_length=1000, verbose_name='Описание')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-id']
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
 
 
 class RecipeCategory(models.Model):
@@ -44,5 +60,13 @@ class RecipeCategory(models.Model):
     ○ *обязательные для связи поля
     ○ *другие поля на ваш выбор
     """
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, verbose_name='Рецепт')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+
+    def __str__(self):
+        return f'{self.recipe} - {self.category}'
+
+    class Meta:
+        ordering = ['-id']
+        verbose_name = 'связь рецепта и категории'
+        verbose_name_plural = 'связи рецепта и категории'
