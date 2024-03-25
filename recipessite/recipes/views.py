51cc30704a29d3251fb4af8b51764e7922427042
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2024-03-25 15:23:28 +1000

    added main page

--- recipessite/recipes/views.py
+++ recipessite/recipes/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+from .models import Recipe, Category, RecipeCategory
 
+"""
+Шаблоны
+Подготовьте базовый шаблон проекта и нижеперечисленные дочерние шаблоны:
+● Главная с 5 случайными рецептами кратко
+● Страница с одним подробным рецептом
+● Страницы регистрации, авторизации и выхода пользователя
+● Страница добавления/редактирования рецепта
+● *другие шаблоны на ваш выбор
+"""
 
-# Create your views here.
 
 def index(request):
-    return render(request, 'recipes/index.html')
+    """
+    ● Главная с 5 случайными рецептами кратко
+    """
+    recipes = Recipe.objects.order_by('?')[:5]
+    context = {'recipes': recipes,
+               'title': 'Главная',
+               }
+    return render(request, 'recipes/index.html', context)
