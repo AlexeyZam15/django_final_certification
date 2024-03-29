6feeda4d81372c71f87674a683f9e1670ba71bb9
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2024-03-29 21:42:38 +1000

    replaced login and register views, changed static css dir

--- recipessite/users/urls.py
+++ recipessite/users/urls.py
@@ -1,9 +1,10 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path('login/', views.login_user, name='login'),
-    path('logout/', views.logout_user, name='logout'),
-    path('register/',views.register, name='register'),
+    path('login/', views.LoginUser.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', views.RegisterUser.as_view(), name='register'),
 ]
